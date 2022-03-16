6d5b75407e1bec24329d175f051f29895ecb1d4c
Rodrigo Valori <rodpalinkas@gmail.com>
2022-03-16 21:44:55 +0100

    creating getters and setters + constructor for some new attributes

--- src/main/java/br/com/fiap/teste/TestePersist.java
+++ src/main/java/br/com/fiap/teste/TestePersist.java
@@ -26,27 +26,27 @@ public class TestePersist {
 			Consumidor consumidor = new Consumidor(1123456789l, "Ciclano", "ciclano@gmail.com",
 					new GregorianCalendar(2027, Calendar.DECEMBER, 1), 1234567l, "Homem", 19876, "Rua Teste, 10",
 					"Bairro", "Cidade", "Estado");
-			
+
 			Comanda comanda = new Comanda(Calendar.getInstance(), 257.10, consumidor);
-			
+
 			List<Consumo> consumos = new ArrayList<Consumo>();
-			
-			Consumo consumo1 = new Consumo(2.3, 1.5, Estilo.LAGER, Marca.AMSTEL, consumidor, comanda);
+
+			Consumo consumo1 = new Consumo(2.7, 1.0, 1.9, Estilo.STOUT, Marca.AMSTEL, consumidor, comanda);
 			consumos.add(consumo1);
-			Consumo consumo2 = new Consumo(2.6, 2.0, Estilo.IPA, Marca.BRAHMA, consumidor, comanda);
+			Consumo consumo2 = new Consumo(0.4, 0.9, 2.3, Estilo.PORTER, Marca.BUDWEISER, consumidor, comanda);
 			consumos.add(consumo2);
-			Consumo consumo3 = new Consumo(1.0, 1.5, Estilo.LAGER, Marca.AMSTEL, consumidor, comanda);
+			Consumo consumo3 = new Consumo(1.3, 7.1, 4.9, Estilo.TRIPEL, Marca.COLORADO, consumidor, comanda);
 			consumos.add(consumo3);
 
 			em.getTransaction().begin();
 
 			em.persist(consumidor);
 			em.persist(comanda);
-			
+
 			for (Consumo consumo : consumos) {
 				em.persist(consumo);
 			}
-			
+
 			em.getTransaction().commit();
 
 		} catch (Exception e) {
