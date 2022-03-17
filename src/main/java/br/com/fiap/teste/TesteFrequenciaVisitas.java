c025c8b8971b20d6dccf5ae61f6d65a704f9dec7
Rodrigo Valori <rodpalinkas@gmail.com>
2022-03-17 23:45:52 +0100

    adding new teste class

---
+++ src/main/java/br/com/fiap/teste/TesteFrequenciaVisitas.java
@@ -0,0 +1,40 @@
+package br.com.fiap.teste;
+
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class TesteFrequenciaVisitas {
+
+	public static void main(String[] args) {
+
+		EntityManager em = null;
+
+		try {
+			em = Persistence.createEntityManagerFactory("enjoyIt").createEntityManager();
+
+			String jpql = "select COUNT(c.dataComanda) from Comanda c, Consumidor s where c.dataComanda between '01/03/2022 23:51:35,545000000' and '31/03/2022 23:51:35,545000000' and s.numeroTelefone = 1123456789";
+
+			TypedQuery<Long> typedQuery = em.createQuery(jpql, Long.class);
+
+			Long comanda = typedQuery.getSingleResult();
+
+			System.out.println(comanda);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			if (em != null && em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+
+			}
+		}
+	}
+}
